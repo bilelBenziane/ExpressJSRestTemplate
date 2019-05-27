cc42e0a75eeda714447a52f8dc2b2da9db271196
bilelBenziane <bilel.benziane2016@gmail.com>
2019-05-27 09:53:37 +0100

    handling CORS

--- app.js
+++ app.js
@@ -15,6 +15,16 @@ app.use(morgan('dev'));
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+
+  if (req.method === 'OPTIONS') {
+    res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
+    res.status(200).json({});
+  }
+});
+
 //  app.use(bodyParser.urlencoded({ extended: false }));
 //  app.use(bodyParser.json());
 
